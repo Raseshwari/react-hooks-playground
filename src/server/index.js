d6a3245d87386642e35d40958552ace0473ed9bc
Raseshwari <32209693+Raseshwari@users.noreply.github.com>
2020-09-05 01:18:04 -0400

    db config

--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,40 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("../routes/api");
+const dbCon = require('../constants/db');
+require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+mongoose
+  .connect(dbCon, { useNewUrlParser: true })
+  .then(() => console.log("connected to DB successfully"))
+  .catch((err) => console.log(err));
+
+//since mongoose promise is depreciated, we overide it with node's promise
+mongoose.Promise = global.Promise;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-app.use((req, res, next)=>{
-    res.send('Welcome to Express');
+app.use(bodyParser.json);
+
+app.use("/api", routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
