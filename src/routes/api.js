d181f3a40c853bfd4d788f5166e49c392ded7563
Raseshwari <32209693+Raseshwari@users.noreply.github.com>
2020-09-05 01:18:50 -0400

    db config

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,31 @@
-const express = require('express');
+const express = require("express");
+const Todo = require("../models/todo");
 const router = express.Router();
 
-router.get('/todos', (req, res, next)=>{
-
+router.get("/todos", (req, res, next) => {
+  Todo.find({}, "title")
+    .then((data) => res.json(data))
+    .catch(next);
 });
 
-router.post('/todos', (req, res, next) => {
-
+router.post("/todos", (req, res, next) => {
+  if (req.body.title) {
+    Todo.create(req.body)
+      .then((data) => res.json(data))
+      .catch(next);
+  } else {
+    res.json({
+      error: "Todo field cannot be empty",
+    });
+  }
 });
 
-router.delete('/todos/:id', (req, res, next) => {
-
+router.delete("/todos/:id", (req, res, next) => {
+  Todo.findOneAndDelete({ _id: req.params.id })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch(next);
 });
 
 module.exports = router;
