ca04dfafb0e7a0bb953b721c0bb5a59c6b83d7ba
Raseshwari <32209693+Raseshwari@users.noreply.github.com>
2020-09-04 23:39:39 -0400

    code refactor

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,7 @@
 import React, { useState } from "react";
+import Task from './Task';
 import CreateTask from "./CreateTask";
 import "./Todo.css";
-
-function Task({ task, index, completeTask, removeTask }) {
-  return (
-    <div
-      className="task"
-      style={{ textDecoration: task.completed ? "line-through" : "" }}
-    >
-      {task.title}
-      <button style={{ background: "red" }} onClick={() => removeTask(index)}>x</button>
-      <button onClick={() => completeTask(index)}>Complete</button>
-    </div>
-  );
-}
 
 function Todo() {
   const [tasks, setTasks] = useState([
